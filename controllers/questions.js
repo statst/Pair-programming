e4d4f4e3c46f1784eb0b729006aab588a911ec6c
foolan <foolanbhosale5@gmail.com>
2020-05-09 01:44:40 -0400

    Working form

--- controllers/questions.js
+++ controllers/questions.js
@@ -1,6 +1,6 @@
-const Question = require('../models/schema');
 const express = require('express');
 const router = express.Router();
+const Question = require('../models/schema');
 
 router.get('/', (req, res) => {
     Question.find({})
@@ -11,43 +11,54 @@ router.get('/', (req, res) => {
 });
 
 router.get('/new', (req, res) => {
-	res.render('new');
+    res.render('new');
 });
-
 
 router.get('/edit/:id', (req, res) => {
-	const id = req.params.id;
+    const id = req.params.id;
     Question.findById(id)
-    .then((question) => {
-		res.render('edit', question);
-	});
+        .then((question) => {
+            res.render('edit', question);
+        })
+        .catch(console.error);
 });
-
 
 router.get('/:id', (req, res) => {
     const id = req.params.id;
-    Question.findById(id).then((question) => {
-        console.log(question);
-        res.render('show', question);
-    });
-});
-router.post('/', (req, res) => {
-    Question.create(question)
-    .then((question) => {
-        res.redirect('/questions');
-    });
-});
-
-router.put('/:id', (req, res) => {
-    const id = req.params.id;
-		Question.findOneAndUpdate({ _id: id }, req.body, {
-			new: true,
+    Question.findById(id)
+        .then((question) => {
+            res.render('show', question);
         })
-        .then((question) => res.redirect('/questions'));
+        .catch((err) => console.log(err));
 });
 
+router.post('/', (req, res) => {
+    const question = req.body;
+    Question.create(question).then((question) => res.redirect('/questions')).catch((err) => console.log(err));
+});
 
+router.put('/edit/:id', (req, res) => {
+    const id = req.params.id;
+    Question.findOneAndUpdate({_id: id}, req.body, {
+        new: true
+    }).then((question) => res.redirect('/questions'));
+});
 
+router.delete('/edit/:id', (req, res) => {
+    Question.findOneAndDelete({_id: req.params.id}).then(res.redirect('/questions'));
+});
 
-// router.get()
+router.post('/:id', (req, res) => {
+    Question.findById(req.params.id)
+        .then((question) => {
+            question.answer.push(req.body);
+            question.save(function() {
+                res.redirect('/questions/id');
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
 module.exports = router;
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+
+const questionsController = require('./controllers/questions');
 
 app.set('view engine', 'hbs');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 const methodOverride = require('method-override');
 
 app.use(methodOverride('_method'));
-
-const questionsController = require('./controllers/questions');
 
 app.use('/questions', questionsController);
 
